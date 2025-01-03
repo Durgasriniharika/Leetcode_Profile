dd41336ba23be42b83bf6345983519af920e944a
Durga sri niharika <142823558+Durgasriniharika@users.noreply.github.com>
2025-01-03 20:53:01 +0530

    Time: 0 ms (100%), Space: 35.8 MB (40.29%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total=0;
+        for(auto it:nums){
+            total+=it;
+        }
+        int prefix=0;
+        int idx=-1;
+        for(int i=0;i<nums.size();i++){
+           
+            int suffix=total-prefix-nums[i];
+            if(prefix==suffix){
+                return i;
+            }
+             prefix+=nums[i];
+        }
+        return -1;
+    }
+};
