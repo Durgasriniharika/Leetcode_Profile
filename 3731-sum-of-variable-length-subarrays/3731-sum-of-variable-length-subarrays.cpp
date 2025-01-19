46c166c60156ea98c363cf77fdc48516ded9f38f
Durga sri niharika <142823558+Durgasriniharika@users.noreply.github.com>
2025-01-19 23:22:04 +0530

    Time: 0 ms (100%), Space: 27.5 MB (36.36%) - LeetHub

--- 3731-sum-of-variable-length-subarrays/3731-sum-of-variable-length-subarrays.cpp
+++ 3731-sum-of-variable-length-subarrays/3731-sum-of-variable-length-subarrays.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>prefix(n+1,0);
+        for(int i=0;i<n;i++){
+            prefix[i+1]=prefix[i]+nums[i];
+        }
         int total_sum=0;
-        for(int i=0;i<nums.size();i++){
+        for(int i=0;i<n;i++){
             int start=max(0,i-nums[i]);
-            for(int j=start;j<=i;j++){
-                total_sum+=nums[j];
-            }
+            total_sum+=prefix[i+1]-prefix[start];
         }
         return total_sum;
     }
