39c794c2c03e42800db6191a46872ea0128204a4
Durga sri niharika <142823558+Durgasriniharika@users.noreply.github.com>
2025-03-03 20:59:21 +0530

    Time: 3 ms (88.52%), Space: 127.8 MB (77.25%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int>mid;
-        vector<int>less;
-        vector<int>great;
+        int left=0;
+        vector<int>res(nums.size());
+        for(auto num:nums){
+           if(num<pivot){
+                res[left++]=num;
+           }
+        }
         for(auto num:nums){
             if(num==pivot){
-                mid.push_back(num);
+                res[left++]=num;
             }
         }
         for(auto num:nums){
-            if(num<pivot){
-                less.push_back(num);
-            }
+           if(num>pivot){
+                res[left++]=num;
+           }
         }
-        for(auto num:nums){
-            if(num>pivot){
-                great.push_back(num);
-            }
-        }
-        vector<int>res;
-        res.insert(res.end(),less.begin(),less.end());
-        res.insert(res.end(),mid.begin(),mid.end());
-        res.insert(res.end(),great.begin(),great.end());
         return res;
     }
 };
