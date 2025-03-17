32351e3ba542e8d746fee4e174dbb6db102a4c56
Durga sri niharika <142823558+Durgasriniharika@users.noreply.github.com>
2025-03-17 12:05:15 +0530

    Time: 0 ms (100%), Space: 8.6 MB (5.9%) - LeetHub

--- 2481-strictly-palindromic-number/2481-strictly-palindromic-number.cpp
+++ 2481-strictly-palindromic-number/2481-strictly-palindromic-number.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
-    bool isStrictlyPalindromic(int n) {
+    bool ispalindrome(string n){
+        string temp=n;
+        reverse(n.begin(),n.end());
+        if(temp==n) return true;
         return false;
+    }
+    string solve(int n,int base){
+        if(n==0) return "0";
+        vector<int>all_bases;
+        
+        while(n>0){
+            all_bases.push_back(n%base);
+            n/=base;
+        }
+
+        string res="";
+        for(int i=all_bases.size()-1;i>=0;i--){
+            res+=to_string(all_bases[i]);
+        }
+        return res;
+    }
+    bool isStrictlyPalindromic(int n) {
+        for(int base=2;base<=n-2;base++){
+            string num=solve(n,base);
+            if(!ispalindrome(num)){
+                return false;
+            }
+        }
+        return true;
     }
 };
