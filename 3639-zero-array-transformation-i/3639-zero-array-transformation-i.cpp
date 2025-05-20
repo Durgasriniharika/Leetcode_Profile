3e7e5bc0e41b69936a3d704147453cb9876c8675
Durga sri niharika <142823558+Durgasriniharika@users.noreply.github.com>
2025-05-21 00:03:34 +0530

    Time: 2 ms (90.04%), Space: 294.6 MB (41.5%) - LeetHub

--- 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
+++ 3639-zero-array-transformation-i/3639-zero-array-transformation-i.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        //1. count the apperance of each index
+        //2. calculate the prefix sum for the count array
+        //3. check the count array with the nums array 
+        // that any index number count is < that number in nums return false otherwisw true
         int n=nums.size();
         vector<long long>cnt(n+1,0);
         for(int i=0;i<queries.size();i++){
                 int first=queries[i][0];
                 int last=queries[i][1];
+                //difference array method for the given query range
                 cnt[first]+=1;
                 if(last+1<n){
                     cnt[last+1]-=1;
                 }
         }
+        //prefix sum for count array
         for(int i=1;i<n;i++){
             cnt[i]+=cnt[i-1];
         }
