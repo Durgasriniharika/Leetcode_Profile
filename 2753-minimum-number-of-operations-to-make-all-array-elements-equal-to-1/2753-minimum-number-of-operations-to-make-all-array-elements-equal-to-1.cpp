cfb669f71349d42c4fd45db11eedc99a8dd8b75c
Durga sri niharika <142823558+Durgasriniharika@users.noreply.github.com>
2025-11-12 23:30:27 +0530

    Time: 0 ms (100%), Space: 31.6 MB (72.22%) - LeetHub

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -2,15 +2,21 @@ class Solution {
 public:
     int minOperations(vector<int>& nums) {
         int n=nums.size();
+
+        //1. Calculate gcd of complete array
         int gcd_all=nums[0];
         for(int i=1;i<n;i++){
             gcd_all=__gcd(gcd_all,nums[i]);
         }
+        //if it doesn't contain single one then it is impossible to make it one for any subarray so return -1
         if(gcd_all>1) return -1;
 
+        //2. Calculate no.of ones present intially in the given array in order to make other numbers to one
         int cnt=count(nums.begin(),nums.end(),1);
+        //to get the remaining non-one numbers that is equal to min_opertion cnt
         if(cnt>0) return n-cnt;
 
+        //3. Calculate the min_op cnt to make the complete array to ones
         int ans=INT_MAX;
         for(int i=0;i<n;i++){
             int m=nums[i];
@@ -22,6 +28,7 @@ public:
                 }
             }
         }
+        //remaining check
         return ans+n-1;
     }
 };
