b53669695ab7af22e19808349fbae6b0870f8417
Durga sri niharika <142823558+Durgasriniharika@users.noreply.github.com>
2026-01-04 21:13:32 +0530

    Time: 3 ms (95.07%), Space: 16.1 MB (71.43%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    bool isPrime(int x){
-        if(x<2) return false;
-        for(int i=2;i*i<=x;i++){
-            if(x%i==0) return false;
-        }
-        return true;
-    }
     int sumFourDivisors(vector<int>& nums) {
-        int sum=0;
+        int total_sum=0;
         for(auto it:nums){
-            //case1: cube of a prime number(1,p,p^2,p^3)
-            int p=round(cbrt(it));
-            if(p*p*p==it && isPrime(p)){
-                sum+=(1+p+p*p+it);
-                continue;
-            }
-
-            //case2: product of two distinct primes(1,p,q,p*q)
-            for(int i=2;i*i<=it;i++){
+            int cnt=0;
+            int sum=0;
+            for(int i=1;i*i<=it;i++){
                 if(it%i==0){
-                    int j=it/i;
-                    if(i!=j && isPrime(i) && isPrime(j)){
-                        sum+=(1+i+j+it);
+                    int n1=i;
+                    int n2=it/i;
+
+                    cnt++;
+                    sum+=i;
+
+                    if(n1!=n2){
+                        cnt++;
+                        sum+=n2;
                     }
-                    break;
+                    if(cnt>4) break;
                 }
             }
+            if(cnt==4){
+                total_sum+=sum;
+            }
         }
-        return sum;
+        return total_sum;
     }
 };
